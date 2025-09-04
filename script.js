dff9826cb4a6bedcd0c2b94c958adfbee77bc8ea
kxnn02 <fernandezk728@gmail.com>
2025-09-04 12:48:53 +0800

    Finished writing the pseudocode and algorithm for the program

---
+++ script.js
@@ -0,0 +1,27 @@
+// Create a function called getComputerChoice
+// Generate a random number between 0 and 2 
+// Assign it to a variable called computerChoice
+// If computerChoice === 0 return "Rock" 
+// Else if computerChoice === 1 return "Paper"
+// Else if computerChoice === 2 return "Scissors"  
+
+// Create a function called getHumanChoice
+// Ask the user if ("Rock", "Paper", "Scissors") and assign it to a variable called humanChoice
+// Make the humanChoice case insensitive 
+// return the result
+
+// Logic to play the entire game
+// Create a function called playGame
+// Call the playRound function 5 times to play 5 round
+
+// Track the players score 
+// Create a variable for humanScore and computerScore
+// Initialize the value to 0
+
+// Logic to play single round
+// Create a function called playRound with two parameters (humanChoice, computerChoice)
+// If humanChoice === computerChoice then its a tie 
+// Else if humanChoice beats computerChoice then human wins
+// Else computer wins 
+// Return the result 
+
