1c50656dd70171dd1c7bbe1aeddef7859fbf992c
kxnn02 <fernandezk728@gmail.com>
2025-09-04 13:07:34 +0800

    created the function getComputerChoice and getHumanChoice

--- script.js
+++ script.js
@@ -5,10 +5,27 @@
 // Else if computerChoice === 1 return "Paper"
 // Else if computerChoice === 2 return "Scissors"  
 
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice === 0) {
+        return "ROCK";
+    }else if (computerChoice === 1) {
+        return "PAPER";
+    }else return "SCISSORS";
+}
+
+
 // Create a function called getHumanChoice
 // Ask the user if ("Rock", "Paper", "Scissors") and assign it to a variable called humanChoice
 // Make the humanChoice case insensitive 
 // return the result
+
+function getHumanChoice() {
+    let humanChoice = prompt("Choose: Rock, Paper, or Scissors?");
+    return humanChoice.toUpperCase();
+
+}
+
 
 // Logic to play the entire game
 // Create a function called playGame
@@ -17,6 +34,8 @@
 // Track the players score 
 // Create a variable for humanScore and computerScore
 // Initialize the value to 0
+
+
 
 // Logic to play single round
 // Create a function called playRound with two parameters (humanChoice, computerChoice)
